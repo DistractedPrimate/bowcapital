5b3677bf36bd38691b79b8e93c630d34071456fe
EagleRocker <38844247+EagleRocker@users.noreply.github.com>
2020-01-21 20:02:38 -0800

    Add files via upload

---
+++ scrape3.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+from bs4.element import Tag
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('/Users/max/desktop/chromedriver')
+google_query = 'site:linkedin.com/in/ AND "UC Santa Barbara" AND "Founder"'
+driver.get('https://www.google.com/')
+time.sleep(5)
+search_bar = driver.find_element_by_name('q')
+search_bar.send_keys(google_query)
+search_bar.send_keys(Keys.RETURN)
+time.sleep(3)
+
+soup = BeautifulSoup(driver.page_source,'lxml')
+result_div = soup.find_all('div', attrs={'class': 'g'})
+
+
+links = []
+titles = []
+descriptions = []
+page = 0
+while page < 5:
+    for r in result_div:
+        # Checks if each element is present, else, raise exception
+        try:
+            link = r.find('a', href=True)
+            title = None
+            title = r.find('h3')
+
+            if isinstance(title,Tag):
+                title = title.get_text()
+
+            description = None
+            description = r.find('span', attrs={'class': 'st'})
+
+            if isinstance(description, Tag):
+                description = description.get_text()
+
+            # Check to make sure everything is present before appending
+            if link != '' and title != '' and description != '':
+                links.append(link['href'])
+                titles.append(title)
+                descriptions.append(description)
+        # Next loop if one element is not present
+        except Exception as e:
+            print(e)
+            continue
+    driver.find_element_by_link_text("Next").click()
+    time.sleep(3)
+    page = page +1
+
+#print(titles)
+print(links)
+#print(descriptions)
